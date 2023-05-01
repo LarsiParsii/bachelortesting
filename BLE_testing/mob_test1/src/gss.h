b64635e9e435f7ba564435347928040ddb7c5ab7
Lars Fredrik <lars-on@hotmail.com>
2023-05-01 17:31:25 +0200

    yes

--- BLE_testing/mob_test1/src/gss.h
+++ BLE_testing/mob_test1/src/gss.h
@@ -12,23 +12,49 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 
-static struct characteristic_s {
-    static struct bt_uuid_128 
-}
-
 /* Custom Service Variables */
-#define BT_UUID_CUSTOM_SERVICE_VAL \
-    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+/** @brief GSS Service UUID. */
+#define BT_UUID_GSS_VAL \
+	BT_UUID_128_ENCODE(0x6dea7370, 0x0bdf, 0x43db, 0x8ea8, 0x1c3a6a7e5909)
 
-static struct bt_uuid_128 vnd_uuid = BT_UUID_INIT_128(
-    BT_UUID_CUSTOM_SERVICE_VAL);
+/** @brief GPS Characteristic UUID. */
+#define BT_UUID_GSS_GPS_VAL \
+	BT_UUID_128_ENCODE(0x6dea7371, 0x0bdf, 0x43db, 0x8ea8, 0x1c3a6a7e5909)
 
-static struct bt_uuid_128 vnd_enc_uuid = BT_UUID_INIT_128(
-    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef1));
+/** @brief Humidity Sensor Characteristic UUID. */
+#define BT_UUID_GSS_HUM_VAL \
+	BT_UUID_128_ENCODE(0x6dea7372, 0x0bdf, 0x43db, 0x8ea8, 0x1c3a6a7e5909)
 
-static struct bt_uuid_128 vnd_auth_uuid = BT_UUID_INIT_128(
-    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef2));
+#define BT_UUID_GSS           BT_UUID_DECLARE_128(BT_UUID_GSS_VAL)
+#define BT_UUID_GSS_GPS       BT_UUID_DECLARE_128(BT_UUID_GSS_GPS_VAL)
+#define BT_UUID_GSS_HUM       BT_UUID_DECLARE_128(BT_UUID_GSS_HUM_VAL)
 
-#define BT_UUID_LBS           BT_UUID_DECLARE_128(BT_UUID_LBS_VAL)
-#define BT_UUID_LBS_BUTTON    BT_UUID_DECLARE_128(BT_UUID_LBS_BUTTON_VAL)
-#define BT_UUID_LBS_LED       BT_UUID_DECLARE_128(BT_UUID_LBS_LED_VAL)
+
+/** @brief Callback type for when an LED state change is received. */
+typedef void (*led_cb_t)(const bool led_state);
+
+/** @brief Callback type for when the button state is pulled. */
+typedef bool (*button_cb_t)(void);
+
+/** @brief Callback struct used by the LBS Service. */
+struct gss_cb_s {
+	/** LED state change callback. */
+	led_cb_t    led_cb;
+	/** Button read callback. */
+	button_cb_t button_cb;
+};
+
+/** @brief Initialize the LBS Service.
+ *
+ * This function registers application callback functions with the My LBS
+ * Service 
+ *
+ * @param[in] callbacks Struct containing pointers to callback functions
+ *			used by the service. This pointer can be NULL
+ *			if no callback functions are defined.
+ *
+ *
+ * @retval 0 If the operation was successful.
+ *           Otherwise, a (negative) error code is returned.
+ */
+int my_lbs_init(struct my_lbs_cb *callbacks);
